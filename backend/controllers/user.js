903bfdab21eb33e8090d4260817757e11bcafafa
Stephane BOCHU <stephil06@laposte.net>
2023-02-07 19:20:30 +0100

    MAJ controllers/user.js : MAJ POST /api/auth/login : changer le RANDOM_TOKEN_SECRET ; & quand les identifiants sont faux mettre un message erreur qui ne donne pas d indice sur l email

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -28,8 +28,8 @@ const User = require('../models/user'); // importer le model User
 // signup : inscrire l'utilisateur
 
 
- /* Retourne true Ssi le mot de passe pwd passé en argument est fort
-  cf. https://askcodez.com/expression-reguliere-pour-la-validation-du-mot-de-passe.html
+/* Retourne true Ssi le mot de passe pwd passé en argument est fort
+ cf. https://askcodez.com/expression-reguliere-pour-la-validation-du-mot-de-passe.html
 */
 const isMotDePasseFort = (pwd) => {
   // Le Mot de passe est fort : Il doit contenir minimum 8 caractères & au moins 1 lettre alphabétique minuscule & au moins 1 majuscule 
@@ -75,19 +75,30 @@ Body raw JSON
  }
 puis SEND
 */
+
+/* - Retourne { "userId" ; "token" } 
+  où userId désigne le _id du document Users (Base de Données) 
+  où token désigne le token généré à partir de la variable d'environnement RANDOM_TOKEN_SECRET (chaîne secrète pour chiffrer le token)
+      (le token généré est valable 24H).
+  - Retourne { "erreur": "Login (email; password) incorrect !"} Si identifiants incorrects
+*/
 exports.login = (req, res, next) => {
   console.log(req.body);
 
   User.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
-        return res.status(401).json({ erreur: "L'utilisateur est inconnu dans la base de données" }); // erreur401 Unauthorized
+        // Quand les identifiants sont faux, mettre un message d'erreur qui ne donne pas d'indice sur l'email
+        // return res.status(401).json({ erreur: "L'utilisateur est inconnu dans la base de données" } ); 
+        return res.status(401).json({ erreur: "Login (email; password) incorrect !" } ); // erreur401 Unauthorized
       }
       // fonction compare de bcrypt pour comparer le mot de passe entré par l'utilisateur avec le hash enregistré dans la base de données
       bcrypt.compare(req.body.password, user.password)
         .then(valid => {
           if (!valid) {
-            return res.status(401).json({ erreur: "Mot de passe différent" });
+            // Quand les identifiants sont faux, mettre un message d'erreur qui ne donne pas d'indice sur l'email
+            // return res.status(401).json({ erreur: "Mot de passe différent" });
+            return res.status(401).json({ erreur: "Login (email; password) incorrect !" });
           }
 
           res.status(200).json({
@@ -96,7 +107,8 @@ exports.login = (req, res, next) => {
             token: jwt.sign(
               { userId: user._id },
               // chaîne secrète pour chiffrer notre token
-              'RANDOM_TOKEN_SECRET',
+              // 'RANDOM_TOKEN_SECRET',
+              process.env.RANDOM_TOKEN_SECRET,
               //  durée de validité du token à 24 heures
               { expiresIn: '24h' }
             )
