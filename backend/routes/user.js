0c23d4afbb5249c9b7327da676d32978f27e9dbd
Stephane BOCHU <stephil06@laposte.net>
2023-02-22 09:45:04 +0100

    MAJ routes/user.js: MAJ commentaires

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,19 +2,11 @@ const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/user");
 
-// const auth = require('../middleware/auth');
-// const apiLimiter = require('../middleware/limiter');
-
 router.post('/signup', userCtrl.signup);
 
 const limit = require("../middleware/limiter");
 
 // via limit.loginLimiter : interdire le visiteur à se connecter plus de 3 fois en 5 minutes
-router.post('/login', limit.loginLimiter,
- /* (req, res) => {
-	// your logic
-	res.send("L'utilisateur est connecté");
- }, */
-userCtrl.login);
+router.post('/login', limit.loginLimiter, userCtrl.login);
 
 module.exports = router;
